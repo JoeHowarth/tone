84b93070c1d39dafbf06504af05c5dc4b69ccdaf
pjain03 <pulkit.jain@tufts.edu>
2017-10-13 18:39:56 -0400

    Explaining modules for discussion later

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+var path = require('path'); // working with file and directory paths
+var favicon = require('serve-favicon'); // caches favicon
+var logger = require('morgan'); // Generates logs
+var cookieParser = require('cookie-parser'); 
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+var index = require('./routes/index'); 
 var users = require('./routes/users');
 var app = express();
 
