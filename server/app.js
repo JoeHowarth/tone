de47a968a257998949e3ae75293ae2eceb0fe5a8
pjain03 <pulkit.jain@tufts.edu>
2017-10-13 19:36:57 -0400

    scripts/server

--- server/app.js
+++ server/app.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var path = require('path'); // working with file and directory paths
-var favicon = require('serve-favicon'); // caches favicon
-var logger = require('morgan'); // Generates logs
+// var favicon = require('serve-favicon'); // caches favicon
 var cookieParser = require('cookie-parser'); 
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index'); 
-var users = require('./routes/users');
+// var users = require('./routes/users');
 var app = express();
 
 // view engine setup
@@ -15,14 +14,13 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
