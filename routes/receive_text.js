1aed900897c63fb1c8e49b4064d9f26be5e1679f
pjain03 <pulkit.jain@tufts.edu>
2017-10-13 23:44:58 -0400

    reading body of request

--- routes/receive_text.js
+++ routes/receive_text.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var request = require('request')
 var router = express.Router();
+var bodyparser = require('body-parser');
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded());
+app.use(bodyparser.urlencoded({ extended: true }));
 
 // POST route for receiving text from front-end
 router.post('/send', function(req, res) {
-    res.send(req);
-    // request('', function(error, response, body) {
+    console.log(req.body);
+    res.send(req.body);
+    // request('https://language.googleapis.com/v1/documents' +
+    //     ':analyzeEntities?key=API_KEY', function(error, response, body) {
     //     console.log(error);
     //     console.log(response);
     //     console.log(body);
