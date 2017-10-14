9a9e17bf9132f1e5e78b1be9399f425623a73173
pjain03 <pulkit.jain@tufts.edu>
2017-10-13 23:32:21 -0400

    testing

--- routes/receive_text.js
+++ routes/receive_text.js
@@ -5,11 +5,17 @@ var router = express.Router();
 // POST route for receiving text from front-end
 router.post('/send', function(req, res) {
     console.log(req);
+    res.send("LOL");
     // request('', function(error, response, body) {
     //     console.log(error);
     //     console.log(response);
     //     console.log(body);
     // });
+});
+
+router.get('/send', function(req, res){
+   console.log(req);
+   res.send("lol");
 });
 
 module.exports = router;
