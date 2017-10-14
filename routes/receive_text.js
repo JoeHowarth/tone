41b1f1a6ca32f5388b9909551b5e7b6b290d0fd6
pjain03 <pulkit.jain@tufts.edu>
2017-10-13 23:36:50 -0400

    making sure app can contact server

--- routes/receive_text.js
+++ routes/receive_text.js
@@ -4,18 +4,12 @@ var router = express.Router();
 
 // POST route for receiving text from front-end
 router.post('/send', function(req, res) {
-    console.log(req);
-    res.send("LOL");
+    res.send(req);
     // request('', function(error, response, body) {
     //     console.log(error);
     //     console.log(response);
     //     console.log(body);
     // });
-});
-
-router.get('/send', function(req, res){
-   console.log(req);
-   res.send("lol");
 });
 
 module.exports = router;
