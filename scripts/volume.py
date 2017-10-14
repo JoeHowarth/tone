1537489652e74a3b304a9b34f017ffd7490a48bd
JoeHowarth <josephehowarth@gmail.com>
2017-10-14 06:12:15 -0400

    vol done?

--- scripts/volume.py
+++ scripts/volume.py
@@ -1,5 +1,6 @@
 from pydub import AudioSegment
-# 
+import sys
+#
 # def match_target_amplitude(sound, target_dBFS):
 #     change_in_dBFS = target_dBFS - sound.dBFS
 #     return sound.apply_gain(change_in_dBFS)
@@ -12,7 +13,11 @@ from pydub import AudioSegment
 #     change_in_dBFS = target_dBFS - sound.dBFS
 #     return sound.apply_gain(change_in_dBFS)
 
-sound = AudioSegment.from_file("yourAudio.m4a", "aac")
-print(sound.rms)
+if len(sys.argv) > 2:
+    path = sys.argv[1]
+    sound = AudioSegment.from_file(path, "aac")
+    vol = (sound.rms)
+    print(vol)
+    sys.stdout.flush()
 # normalized_sound = match_target_amplitude(sound, -20.0)
 # normalized_sound.export("nomrmalizedAudio.m4a", format="mp4")
