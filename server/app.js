bd6e0977003dadb96565823b202f3816246123b0
pjain03 <pulkit.jain@tufts.edu>
2017-10-13 21:50:52 -0400

    Added post route

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var spotifyApp = require('./routes/spotifyApp');
+var receive_text = require('./routes/receive_text');
 // var users = require('./routes/users');
 var app = express();
 
@@ -20,10 +20,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', index);
 // app.use('/users', users);
+app.use('/', receive_text);
 
-app.use('/play', spotifyApp);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
