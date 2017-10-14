7ed151a9f7143a3278a98237e9bea5fe4ea7bf99
pjain03 <pulkit.jain@tufts.edu>
2017-10-13 23:49:52 -0400

    redefining app

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set('config', config);
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
